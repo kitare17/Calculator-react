5a7851d629b4eba7dfe8ca24107fee108a998de3
kitare17 <hoctapak@gmail.com>
2023-05-08 18:16:41 +0700

    Push code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useRef, useState} from 'react';
+
+
 
 function App() {
+   const inputRef=useRef(null);
+    const resultRef=useRef(null);
+
+   const [result,setResult] =useState(0);
+   function plus(e){
+   e.preventDefault(); 
+   setResult((result) => result + Number(inputRef.current.value)); 
+   };
+
+   function subtract(e){
+   e.preventDefault(); 
+   setResult((result) => result - Number(inputRef.current.value)); 
+   };
+   function multiply(e){
+   e.preventDefault(); 
+   setResult((result) => result * Number(inputRef.current.value)); 
+   };
+   function divide(e){
+   e.preventDefault(); 
+   setResult((result) => result / Number(inputRef.current.value)); 
+   };
+  const resetInput= (e) => {
+    e.preventDefault();
+    inputRef.current.value=null;
+  };
+  const  resetResult=()=> {
+    setResult(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+   {result}
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <br></br>
+        <button onClick={plus}>add</button> 
+        <button onClick={subtract}>subtract</button> 
+        <button onClick={multiply}>multiply</button> 
+        <button onClick={divide}>divide</button> 
+        <button onClick={resetInput}>Reset Input</button> 
+        <button onClick={resetResult}>Reset Result</button> 
+        {/* Add the subtract button */} 
+        {/* Add the multiply button */} 
+        {/* Add the divide button */} 
+        {/* Add the resetInput button */} 
+        {/* Add the resetResult button */} 
+      </form> 
+    </div> 
   );
 }
 
